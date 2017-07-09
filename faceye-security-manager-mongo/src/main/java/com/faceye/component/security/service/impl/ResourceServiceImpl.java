34618a8e38ca08885f5076a1f9bdec537b070df1
haipenge@gmail.com <haipenge@gmail.com>
2017-07-09 21:56:02 +0800

    change security versiont 2 4.2.3

--- faceye-security-manager-mongo/src/main/java/com/faceye/component/security/service/impl/ResourceServiceImpl.java
+++ faceye-security-manager-mongo/src/main/java/com/faceye/component/security/service/impl/ResourceServiceImpl.java
@@ -37,7 +37,7 @@ public class ResourceServiceImpl extends BaseMongoServiceImpl<Resource, Long, Re
 	}
 
 	@Override
-	public void remove(Long id) throws ServiceException {
+	public void remove(Long id) {
 		Resource resource = this.get(id);
 		List<Role> roles = this.roleService.getAll();
 		if (CollectionUtils.isNotEmpty(roles)) {
@@ -60,7 +60,7 @@ public class ResourceServiceImpl extends BaseMongoServiceImpl<Resource, Long, Re
 	}
 
 	@Override
-	public void remove(Resource entity) throws ServiceException {
+	public void remove(Resource entity)  {
 		this.remove(entity.getId());
 		;
 	}
